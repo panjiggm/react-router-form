a832726b0d6bbaaa69744098ca269d48c9d72488
Panji Gumelar <panjiggm@gmail.com>
2019-01-12 22:24:42 +0700

    practice life cycle component: DIdMount and WillUnmount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 
-import Register from "./components/register";
+// import Register from "./components/register";
+import Course from "./components/Course";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Register</h1>
-          <Register />
+        <header>
+          <h1>React 101</h1>
+          <Course />
         </header>
       </div>
     );
